a800f59ca27dba173dff42336b95514df136d117
auro <auro@DESKTOP-AV8I9BT>
2024-08-14 17:07:51 +0300

    Add project files.

---
+++ PolymorphismPratice/BaseGeometrikSekil.cs
@@ -0,0 +1,61 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace PolymorphismPratice
+{
+    public class BaseGeometrikSekil
+    {
+        public int Genislik { get; set; }
+        public int Yukseklik { get; set; }
+
+        public virtual double AlanHesapla()
+        {
+            return Genislik * Yukseklik;
+        }
+        public virtual void AlanSonuc()
+        {
+            Console.WriteLine("Alan hesaplanıldı.");
+        }
+    }
+        public class Kare : BaseGeometrikSekil
+        {
+            public override double AlanHesapla()
+            {
+                return Yukseklik * Yukseklik;
+            }
+            public override void AlanSonuc()
+            {
+                Console.WriteLine($"Karenin Alanı: {AlanHesapla()}");
+            }
+
+        }
+        public class Dikdortgen : BaseGeometrikSekil
+        {
+            public override double AlanHesapla()
+            {
+                return base.AlanHesapla();
+            }
+            public override void AlanSonuc()
+            {
+                Console.WriteLine($"Dikdörtgenin Alanı: {AlanHesapla()}");
+            }
+        }
+        public class DikUcgen : BaseGeometrikSekil
+        {
+            public override double AlanHesapla()
+            {
+                return (Yukseklik * Genislik) / 2;
+            }
+            public override void AlanSonuc()
+            {
+                Console.WriteLine($"Dik Üçgenin Alanı: {AlanHesapla()}");
+            }
+        }
+
+
+
+
+     }
---
+++ PolymorphismPratice/Program.cs
@@ -0,0 +1,33 @@
+﻿using PolymorphismPratice;
+
+public class Program
+{
+    public static void Main(string[] args)
+    {
+
+       Kare kare = new Kare();
+        kare.Genislik = 3;
+        kare.Yukseklik = 3;          // Kare alan örneği
+        kare.AlanSonuc();
+
+        Console.WriteLine("------------------------------");
+
+        Dikdortgen dikdortgen = new Dikdortgen();
+        dikdortgen.Genislik = 2;
+        dikdortgen.Yukseklik = 4;        // Dikdörtgen alan örneği
+        dikdortgen.AlanSonuc();
+
+
+        Console.WriteLine("------------------------------");
+
+
+        DikUcgen dikUcgen = new DikUcgen();
+        dikUcgen.Genislik = 2;
+        dikUcgen.Yukseklik = 3;         // Dik Üçgen alan örneği
+        dikUcgen.AlanSonuc();
+
+
+
+
+    }
+}
